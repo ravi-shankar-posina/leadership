194b9c24dc63b73ac376675ad7da87e3784e7337
R sai rajesh <rudrravarjula1234@gmail.com>
2018-04-16 19:59:31 +0530

    Commit1
    The first basic commit
    
    Co-Authored-By: Ravi Shankar <ravishankar.sla@gmail.com>

---
+++ clubs/web.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="page">
+    <div class="header">    
+        <?php
+            include "header.php";
+        ?>    
+    </div>
+    <div>
+        <img src="images/clubs/webapps.jpg" width="100%" height="600px" alt="">
+    </div>
+    <div class="footer">
+        <?php
+            include "footer.php";
+        ?>    
+    </div>
+</div>
+</body>
+</html>
